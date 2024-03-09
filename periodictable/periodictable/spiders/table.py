6ac1ad726c07514a1a98081e284ac6c41b01b9d9
Amias Yaska <amiasyaska@gmail.com>
2024-03-09 19:03:20 +0300

    created spider

--- periodictable/periodictable/spiders/table.py
+++ periodictable/periodictable/spiders/table.py
@@ -3,8 +3,20 @@ import scrapy
 
 class TableSpider(scrapy.Spider):
     name = "table"
-    allowed_domains = ["pubchem.ncbi.nlm.nih.gov"]
-    start_urls = ["https://pubchem.ncbi.nlm.nih.gov"]
+    allowed_domains = ["pubchem.ncbi.nlm.nih.gov/ptable"]
+
+    def start_requests(self):
+
+        yield scrapy.Request(
+            url="https://pubchem.ncbi.nlm.nih.gov/rest/pug/periodictable/JSON",
+            method="GET",
+            # body=json.dumps(query),
+            headers={
+                "Content-Type": "application/json"
+            },
+            callback=self.parse
+        )
 
     def parse(self, response):
-        pass
+        print(response.body)
+
